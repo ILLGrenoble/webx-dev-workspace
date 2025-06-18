e3c82a93d0b677d8504d6f3e59b76d63a5a06235
Stuart Caunt <caunt@ill.fr>
2025-06-18 20:24:36 +0000

    merge webx-session-manager into webx-router

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 mkdir /var/log/webx
-mkdir -p /etc/webx/webx-session-manager
 
 # Copy session manager config files
-cp webx-session-manager/bin/pam-webx /etc/pam.d/webx
-cp webx-session-manager/bin/startwm.sh /etc/webx/webx-session-manager/startwm.sh
-cp webx-session-manager/config.example.yml webx-session-manager/config.yml 
+cp webx-router/bin/pam-webx /etc/pam.d/webx
+cp webx-router/bin/startwm.sh /etc/webx/startwm.sh
 
 # Update default config
-sed -i 's|/etc/X11/xrdp/xorg.conf|xorg.conf|g' webx-session-manager/config.yml 
+sed -i 's|/etc/X11/xrdp/xorg.conf|xorg.conf|g' webx-router/config.yml 
 sed -i 's/startxfce4/dbus-launch startxfce4/g' /etc/webx/webx-session-manager/startwm.sh
 
