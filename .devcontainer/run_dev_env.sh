0da8040a469a31fc6d97e485ac4ee0603ea117fa
Stuart Caunt <caunt@ill.fr>
2026-02-09 13:54:32 +0000

    create a true randr mode rather than just using the framebuffer resize. This allows webx to determine the correct connected screen.

--- .devcontainer/run_dev_env.sh
+++ .devcontainer/run_dev_env.sh
@@ -10,11 +10,22 @@ trap 'cleanup' SIGINT
 
 export XCURSOR_SIZE=24
 
+# Use standard xrdp-xorg
+# /usr/bin/run_wm.sh -w 1920 -h 1080 $@ &
+
 # Run using Xvfb with a large framebuffer
 /usr/bin/run_wm.sh -w 8192 -h 8192 -x Xvfb $@ &
 
 sleep 5
-DISPLAY=:20 xrandr --fb 1920x1080
+export DISPLAY=:20
+WIDTH=1920
+HEIGHT=1080
+
+OUTPUT=$(xrandr --query | awk '/ connected/{print $1; exit}')
+MODE_DETAILS=$(cvt $WIDTH $HEIGHT 60.0 | sed -n 's/Modeline "\(.*\)_.*"\(.*\)/\1\2/p')
+xrandr --newmode $MODE_DETAILS
+xrandr --addmode $OUTPUT ${WIDTH}x${HEIGHT}
+xrandr --output $OUTPUT --mode ${WIDTH}x${HEIGHT}
 
 wait $!
 
